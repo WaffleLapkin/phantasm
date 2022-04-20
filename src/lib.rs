ab0096c1cb99673dc8ffb8da6fc3f7c53e6fa781
Maybe Waffle <waffle.lapkin@gmail.com>
2022-04-20 13:14:14 +0400

    Improve documentation and doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! This crate is aiming to make work with [variance][var] easier.
 //! The crate exposes 3 types - [`Invariant<T>`][inv], [`Covariant<T>`][cov] and
 //! [`Contravariant<T>`][cnt] with corresponding variance over `T` those work
-//! very similar to [`PhantomData<_>`][phd].
+//! in a very similar way to [`PhantomData<_>`][phd].
 //!
 //! [var]: https://doc.rust-lang.org/nomicon/subtyping.html
 //! [inv]: crate::Invariant#type
@@ -28,25 +28,26 @@
 //!   = help: consider removing `'a`, referring to it in a field, or using a marker such as `std::marker::PhantomData`
 //! ```
 //! This is an error because rust compiler doesn't know should `Slice` be
-//! covariant, contravariant or invariant over `'a`. (What it means is that
-//! rustc doesn't know if `Slice<'static, _>` should be `Slice<'a, _>` or vice
-//! versa or none of this. See [Subtyping and Variance][nom] nomicon chapter for
-//! better explanation)
+//! covariant, contravariant or invariant over `'a`. What this means is that
+//! rustc doesn't know if `Slice<'static, _>` should be a subtype of `Slice<'a,
+//! _>` or vice versa or neither. See [Subtyping and Variance][nom] nomicon
+//! chapter for better explanation
 //!
 //! To mitigate this issue and control the variance there is a type called
-//! [`core::marker::PhantomData<T>`][phd]. [`PhantomData<T>`][phd] is a
+//! [`marker::PhantomData<T>`][phd]. [`PhantomData<T>`][phd] is a
 //! zero-sized type that acts like it owns `T`.
 //!
 //! However, [`PhantomData`][phd] comes with a number of issues:
-//! 1. Variance is a hard thing by itself, but [`PhantomData`][phd] makes it
-//!    even harder to understand. It's not straightforward to understand what
-//!    statement like `PhantomData<fn(A, B) -> B>` does (contravariant over `A`
-//!    and invariant over `B`)
+//! 1. Variance is a hard thing to understand by itself, but
+//!    [`PhantomData`][phd] makes it    even harder to understand. It's not
+//!    straightforward to understand what    statement like `PhantomData<fn(A,
+//!    B) -> B>` does (contravariant over `A` and invariant over `B`)
 //! 2. Sometimes it works badly in `const` context (see next
 //!    [paragraph](#function-pointers-in-const-fn-are-unstable))
 //!
-//! `phantasm`'s naming helps with the first by making the original intention
-//! clearer (though variance still is a hard-to-understand thing)
+//! `phantasm`'s naming helps with the first issue by making the original
+//! intention clearer (though variance still is a hard-to-understand thing) and
+//! with the second by doing some _hacks under the hood_.
 //!
 //! [nom]: https://doc.rust-lang.org/nomicon/subtyping.html#subtyping-and-variance
 //!
@@ -55,7 +56,9 @@
 //! It's common practice to make a type invariant over `T` with
 //! `PhantomData<fn(T) -> T>`. However, if you've ever tried to use it in a
 //! `const fn`, you know that it's painful (see [rust-lang/69459][my_issue] and
-//! [rust-lang/67649][or_issue]). This crate helps with this problem:
+//! [rust-lang/67649][or_issue]) because before Rust `1.61.0` function pointers
+//! in `const fn` were unstable, see [stabilization PR] for more. This crate
+//! helps with this problem:
 //!
 //! ```
 //! use phantasm::Invariant;
@@ -64,12 +67,13 @@
 //!
 //! impl<T> Test<T> {
 //!     pub const fn new() -> Self {
-//!         Self(Invariant) // just works
+//!         Self(Invariant) // just works (even on old rust)
 //!     }
 //! }
 //! ```
 //! [my_issue]: https://github.com/rust-lang/rust/issues/69459
 //! [or_issue]: https://github.com/rust-lang/rust/issues/67649
+//! [stabilization PR]: https://github.com/rust-lang/rust/pull/93827
 //!
 //! ## life
 //!
@@ -92,13 +96,16 @@
 //! // won't compile
 //! let _ = phantasm::Invariant<i32>;
 //! ```
+//!
 //! ```
 //! use phantasm::Invariant;
+//!
 //! // ok
 //! let _ = Invariant::<i32>;
-//! // Both forms are acceptable in type possition
-//! struct A<T>(Invariant<T>);
-//! struct B<T>(Invariant<T>);
+//!
+//! // Both forms are acceptable in type position
+//! struct NoFish<T>(Invariant<T>);
+//! struct Turbofish<T>(Invariant<T>);
 //! ```
 //!
 //! ## many types
@@ -108,7 +115,7 @@
 //! struct Test<A, B>(phantasm::Covariant<(A, B)>);
 //! ```
 #![cfg_attr(not(test), no_std)] // `format!` is used in tests
-#![allow(type_alias_bounds)] // for :?Sized bound
+#![allow(type_alias_bounds)] // for :?Sized bound to appear in docs
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
 
@@ -121,23 +128,31 @@
 /// ## Examples
 ///
 /// ```
-/// // This struct is invariant over `T`
-/// struct Test<T>(phantasm::Invariant<T> /* ... */);
+/// use phantasm::Invariant;
 ///
-/// let _: Test<i32> = Test(phantasm::Invariant);
-/// let _ = Test(phantasm::Invariant::<i32>);
+/// // This struct is invariant over `T`
+/// struct Test<T>(Invariant<T> /* ... */);
+///
+/// let _: Test<i32> = Test(Invariant /* ... */);
+/// let _ = Test::<i32>(Invariant /* ... */);
+/// let _ = Test(Invariant::<i32> /* ... */);
 /// ```
 ///
-/// ```compile_fail
-/// // `F<Super>` is **not** a subtype of `F<Sub>`
-/// fn fail<'l>(arg: Invariant<&'l ()>) -> Invariant<&'static ()> {
-///     arg
+/// ```compile_fail,E0308
+/// use phantasm::Invariant;
+///
+/// // `F<Sub>` is **not** a subtype of `F<Super>`
+/// fn covariant_fail<'l>(with_sub: Invariant<&'static ()>) {
+///     let with_super: Invariant<&'l ()> = with_sub; // mismatched types
 /// }
 /// ```
-/// ```compile_fail
-/// // `F<Sub>` is **not** a subtype of `F<Super>`
-/// fn fail<'l>(arg: Invariant<&'static ()>) -> Invariant<&'l ()> {
-///     arg
+///
+/// ```compile_fail,E0308
+/// use phantasm::Invariant;
+///
+/// // `F<Super>` is **not** a subtype of `F<Sub>`
+/// fn contravariant_fail<'l>(with_super: Invariant<&'l ()>) {
+///     let with_sub: Invariant<&'static ()> = with_super; // mismatched types
 /// }
 /// ```
 ///
@@ -158,25 +173,31 @@ pub type Invariant<T: ?Sized> = r#impl::Invariant<T>;
 /// ## Examples
 ///
 /// ```
-/// // This struct is covariant over `T`
-/// struct Test<T>(phantasm::Covariant<T> /* ... */);
+/// use phantasm::Covariant;
 ///
-/// let _: Test<i32> = Test(phantasm::Covariant);
-/// let _ = Test(phantasm::Covariant::<i32>);
+/// // This struct is covariant over `T`
+/// struct Test<T>(Covariant<T> /* ... */);
+///
+/// let _: Test<i32> = Test(Covariant /* ... */);
+/// let _ = Test::<i32>(Covariant /* ... */);
+/// let _ = Test(Covariant::<i32> /* ... */);
 /// ```
 ///
-/// ```compile_fail
+/// ```
 /// use phantasm::Covariant;
-/// // `F<Super>` is **not** a subtype of `F<Sub>`
-/// fn fail<'l>(arg: Covariant<&'l ()>) -> Covariant<&'static ()> {
-///     arg
+///
+/// // `F<Sub>` **is** a subtype of `F<Super>`
+/// fn covariant_pass<'l>(with_sub: Covariant<&'static ()>) {
+///     let with_super: Covariant<&'l ()> = with_sub;
 /// }
 /// ```
-/// ```
+///
+/// ```compile_fail,E0308
 /// use phantasm::Covariant;
-/// // `F<Sub>` **is** a subtype of `F<Super>`
-/// fn pass<'l>(arg: Covariant<&'static ()>) -> Covariant<&'l ()> {
-///     arg
+///
+/// // `F<Super>` is **not** a subtype of `F<Sub>`
+/// fn contravariant_fail<'l>(with_super: Covariant<&'l ()>) {
+///     let with_sub: Covariant<&'static ()> = with_super; // mismatched types
 /// }
 /// ```
 ///
@@ -197,25 +218,31 @@ pub type Covariant<T: ?Sized> = r#impl::Covariant<T>;
 /// ## Examples
 ///
 /// ```
-/// // This struct is covariant over `T`
-/// struct Test<T>(phantasm::Contravariant<T> /* ... */);
+/// use phantasm::Contravariant;
 ///
-/// let _: Test<i32> = Test(phantasm::Contravariant);
-/// let _ = Test(phantasm::Contravariant::<i32>);
+/// // This struct is covariant over `T`
+/// struct Test<T>(Contravariant<T> /* ... */);
+///
+/// let _: Test<i32> = Test(Contravariant /* ... */);
+/// let _ = Test::<i32>(Contravariant /* ... */);
+/// let _ = Test(Contravariant::<i32> /* ... */);
 /// ```
 ///
-/// ```compile_fail
+/// ```compile_fail,E0308
 /// use phantasm::Contravariant;
+///
 /// // `F<Sub>` is **not** a subtype of `F<Super>`
-/// fn fail<'l>(arg: Contravariant<&'static ()>) -> Contravariant<&'l ()> {
-///     arg
+/// fn covariant_fail<'l>(with_sub: Contravariant<&'static ()>) {
+///     let with_super: Contravariant<&'l ()> = with_sub; // mismatched types
 /// }
 /// ```
+///
 /// ```
 /// use phantasm::Contravariant;
+///
 /// // `F<Super>` **is** a subtype of `F<Sub>`
-/// fn pass<'l>(arg: Contravariant<&'l ()>) -> Contravariant<&'static ()> {
-///     arg
+/// fn contravariant_pass<'l>(with_super: Contravariant<&'l ()>) {
+///     let with_sub: Contravariant<&'static ()> = with_super;
 /// }
 /// ```
 ///
@@ -263,6 +290,12 @@ mod r#impl {
 
     /// For documentation see [`Invariant`](crate::Invariant#type)'s docs.
     pub enum Invariant<T: ?Sized> {
+        /// The only possible [`Invariant<_>`][inv] value. For type see
+        /// [`Invariant`][inv] docs.
+        ///
+        /// [inv]: crate::Invariant#type
+        Invariant,
+
         /// Uninhabited variant that uses `T`.
         #[doc(hidden)]
         #[deprecated(
@@ -276,15 +309,16 @@ mod r#impl {
         // `fn(T) -> U` is **contra**variant
         // over `T` and covariant over `U`, so
         // `fn(T) -> T` is invariant over `T`
-        /// The only possible [`Invariant<_>`][inv] value. For type see
-        /// [`Invariant`][inv] docs.
-        ///
-        /// [inv]: crate::Invariant#type
-        Invariant,
     }
 
     /// For documentation see [`Covariant`](crate::Covariant#type)'s docs.
     pub enum Covariant<T: ?Sized> {
+        /// The only possible [`Covariant<_>`][cov] value. For type see
+        /// [`Covariant`][cov] docs.
+        ///
+        /// [cov]: crate::Covariant#type
+        Covariant,
+
         /// Uninhabited variant that uses `T`.
         #[doc(hidden)]
         #[deprecated(
@@ -296,16 +330,17 @@ mod r#impl {
         __Phantom(core::marker::PhantomData<fn(()) -> T>, Never),
         //                                 /^^^^^^^^^^^
         //  `fn(_) -> U` is covariant over `U`
-        /// The only possible [`Covariant<_>`][cov] value. For type see
-        /// [`Covariant`][cov] docs.
-        ///
-        /// [cov]: crate::Covariant#type
-        Covariant,
     }
 
     /// For documentation, see [`Contravariant`](crate::Contravariant#type)'s
     /// docs.
     pub enum Contravariant<T: ?Sized> {
+        /// The only possible [`Contravariant<_>`][cnt] value. For type see
+        /// [`Contravariant`][cnt] docs.
+        ///
+        /// [cnt]: crate::Contravariant#type
+        Contravariant,
+
         /// Uninhibited variant that uses `T`.
         #[doc(hidden)]
         #[deprecated(
@@ -318,13 +353,9 @@ mod r#impl {
         //                                 /^^^^^^^^^^^
         // `fn(T) -> _` is **contra**variant
         // over `T`
-        /// The only possible [`Contravariant<_>`][cnt] value. For type see
-        /// [`Contravariant`][cnt] docs.
-        ///
-        /// [cnt]: crate::Contravariant#type
-        Contravariant,
     }
 
+    // #[derive] doesn't work for us since it adds unnecessary bounds to generics
     macro_rules! impls {
         (for $T:ident) => {
             impl<T: ?Sized> Copy for $T<T> {}
@@ -375,6 +406,7 @@ mod r#impl {
             }
         };
     }
+
     impls!(for Invariant);
     impls!(for Covariant);
     impls!(for Contravariant);
@@ -408,24 +440,24 @@ mod tests {
         assert_eq!(format!("{:?}", Contravariant::<T>), "Contravariant");
     }
 
-    /// ```compile_fail
+    /// ```compile_fail,E0308
     /// use phantasm::Invariant;
-    /// fn fail<'l>(arg: Invariant<&'l ()>) -> Invariant<&'static ()> {
+    /// fn contravariant_fail<'l>(arg: Invariant<&'l ()>) -> Invariant<&'static ()> {
     ///     arg
     /// }
     /// ```
-    /// ```compile_fail
+    /// ```compile_fail,E0308
     /// use phantasm::Invariant;
-    /// fn fail<'l>(arg: Invariant<&'static ()>) -> Invariant<&'l ()> {
+    /// fn covariant_fail<'l>(arg: Invariant<&'static ()>) -> Invariant<&'l ()> {
     ///     arg
     /// }
     /// ```
     #[allow(dead_code)]
     fn invariance() {}
 
-    /// ```compile_fail
+    /// ```compile_fail,E0308
     /// use phantasm::Covariant;
-    /// fn fail<'l>(arg: Covariant<&'l ()>) -> Covariant<&'static ()> {
+    /// fn contravariant_fail<'l>(arg: Covariant<&'l ()>) -> Covariant<&'static ()> {
     ///     arg
     /// }
     /// ```
@@ -437,9 +469,9 @@ mod tests {
         arg
     }
 
-    /// ```compile_fail
+    /// ```compile_fail,E0308
     /// use phantasm::Contravariant;
-    /// fn fail<'l>(arg: Contravariant<&'static ()>) -> Contravariant<&'l ()> {
+    /// fn covariant_fail<'l>(arg: Contravariant<&'static ()>) -> Contravariant<&'l ()> {
     ///     arg
     /// }
     /// ```
